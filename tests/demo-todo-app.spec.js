8035ff1988d58c09582aa24a5f4cf739859838e2
Amanda Bennicasa <amanda@currents.dev>
2023-11-28 17:03:19 -0800

    Playwright Updates

--- tests/demo-todo-app.spec.js
+++ tests/demo-todo-app.spec.js
@@ -82,7 +82,7 @@ test.describe('Mark all as completed', () => {
 
   test('should allow me to mark all items as completed', async ({ page }) => {
     // Complete all todos.
-    await page.getByLabel('Mark all as complete').check();
+    await page.getByLabel('Mark all as incomplete').check();
 
     // Ensure all todos have 'completed' class.
     await expect(page.getByTestId('todo-item')).toHaveClass(['completed', 'completed', 'completed']);
